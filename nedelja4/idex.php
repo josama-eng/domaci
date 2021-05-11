0d3d3aa285a75f4e9ac3cf63c7c4bb17c3eb37a3
Jovana <samardzicjovana27@gmail.com>
2021-05-11 17:40:39 +0200

    Create idex.php

---
+++ nedelja4/idex.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>D11</title>
+</head>
+<body>
+<!-- Zadatak 1 -->
+    <?php 
+    $promenjiva_1 = 200;
+    $promenjiva_2 = 80;
+    echo $promenjiva_1 + $promenjiva_2."<br/>";
+    echo $promenjiva_1 - $promenjiva_2."<br/>";
+    echo $promenjiva_1 * $promenjiva_2."<br/>";
+    echo $promenjiva_1 / $promenjiva_2."<br/>";
+    ?>
+    <!-- Zadatak 2 -->
+    <?php 
+      $a = 5;
+      $b = 3;
+      $c = 8;
+      $zbir = $a + $b + $c;
+      echo "Zbir brojeva $a, $b i $c je $zbir.";
+    ?>
+</body>
+</html>
